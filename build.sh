9d66e5f668fd56fef417b1ec0aaa9133fd8c2dd7
Rob Beal <rob@kohi.uk>
2018-07-23 13:31:16 +0100

    Clean up build.sh as per Tom's suggested improvements

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-arch=($1)
+set -euo pipefail
 
-case ${arch} in
+arch="$1"
+
+case "$arch" in
     i386 ) base_image="resin/i386-alpine" ;;
     amd64 ) base_image="alpine:3.7" ;;
     arm ) base_image="resin/rpi-alpine" ;;
     aarch64 ) base_image="resin/aarch64-alpine" ;;
 esac
-cp Dockerfile Dockerfile.${arch}
-sed -i "s@alpine:\([0-9]\+\).\([0-9]\+\)@$base_image@g" Dockerfile.${arch}
+
+sed "s@alpine:\\([0-9]\\+\\).\\([0-9]\\+\\)@$base_image@g" Dockerfile > "Dockerfile.$arch"
