dafac124279896e10cb7c6c68f896afae84ab9c2
Rob Beal <rob@kohi.uk>
2018-08-16 11:14:55 +0100

    Initial tests for the docker image

--- README.md
+++ README.md
@@ -105,6 +105,16 @@ To customize Radicale configuration, either:
 
 Then puts these two files in a directory and use the config volume `-v /my_custom_config_directory:/config` when running the container.
 
+## Contributing
+
+First install the test dependencies
+
+`pip install --user -r requirements.txt`
+
+To run the tests
+
+`pytest -v test.py`
+
 ## Contributors
 
 * [Robert Beal](https://github.com/robertbeal): fixed/configurable userId, versioning...
---
+++ test.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+import subprocess
+import testinfra
+
+@pytest.fixture(scope='session')
+def host(request):
+    subprocess.check_call(['docker', 'build', '-t', 'radicale', '.'])
+    docker_id = subprocess.check_output(['docker', 'run', '-d', 'radicale']).decode().strip()
+    
+    yield testinfra.get_host("docker://" + docker_id)
+    
+    # teardown
+    subprocess.check_call(['docker', 'rm', '-f', docker_id])
+
+def test_radicale_process(host):
+    assert host.file('/proc/1/cmdline').content_string.replace('\x00','') == '/usr/bin/python3/usr/bin/radicale--config/config/config'
+
+def test_radicale_version(host):
+    assert host.run("/usr/bin/pip3 --disable-pip-version-check show radicale | grep Version | awk -F ' ' '{print $2}' | tr -d '\n'").stdout == os.environ.get('VERSION','2.1.9')
+
+def test_radicale_port(host):
+    assert host.socket("tcp://0.0.0.0:5232").is_listening
+
+def test_radicale_user(host):
+    assert host.user('radicale').uid == 2999
+    assert host.user('radicale').gid == 2999
+
+def test_installed_dependencies(host):
+    assert host.package('curl').is_installed
+    assert host.package('git').is_installed
+    assert host.package('shadow').is_installed
+    assert host.package('su-exec').is_installed
