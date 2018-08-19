ecb42c0c23eafb0c9fb3206697b4f8e5d468a317
Rob Beal <rob@kohi.uk>
2018-08-19 01:17:42 +0100

    Upgrade to alpine 3.8

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.7
+FROM alpine:3.8
 LABEL maintainer="tom@tomsquest.com"
 
 ARG VERSION=2.1.9
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ arch="$1"
 
 case "$arch" in
     i386 ) base_image="resin/i386-alpine" ;;
-    amd64 ) base_image="alpine:3.7" ;;
+    amd64 ) base_image="alpine:3.8" ;;
     arm ) base_image="resin/rpi-alpine" ;;
     aarch64 ) base_image="resin/aarch64-alpine" ;;
 esac
