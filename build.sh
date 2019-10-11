63fbd72c2910f09ddca9da79fa98a72ae87f5918
Thomas Queste <tom@tomsquest.com>
2019-10-11 13:33:58 +0200

    chore: update alpine to 3.10

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 arch="$1"
 
 case "$arch" in
-    amd64   ) base_image="balenalib/amd64-alpine:3.9" ;;
-    i386    ) base_image="balenalib/i386-alpine:3.9" ;;
-    arm     ) base_image="balenalib/armv7hf-alpine:3.9" ;;
-    aarch64 ) base_image="balenalib/aarch64-alpine:3.9" ;;
+    amd64   ) base_image="balenalib/amd64-alpine:3.10" ;;
+    i386    ) base_image="balenalib/i386-alpine:3.10" ;;
+    arm     ) base_image="balenalib/armv7hf-alpine:3.10" ;;
+    aarch64 ) base_image="balenalib/aarch64-alpine:3.10" ;;
 esac
 
 sed "1cFROM $base_image" Dockerfile > "Dockerfile.$arch"
