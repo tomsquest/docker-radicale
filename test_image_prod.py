4331f4f048f46f28bdae88299ce12379a6b12e5b
Thomas Queste <tom@tomsquest.com>
2020-04-30 18:02:50 +0200

    test: add test with standard image and some prod options

--- test_image_prod.py
+++ test_image_prod.py
@@ -1,44 +1,59 @@
-import os
-import pytest
 import subprocess
+
+import pytest
 import testinfra
+
 
 @pytest.fixture(scope='session')
 def host(request):
-    subprocess.check_call(
-        ['docker', 'build', '-t', 'radicale-under-test', '--build-arg', 'VERSION=2.1.11', '--build-arg',
-         'BUILD_UID=6666', '--build-arg', 'BUILD_GID=7777', '.'])
-    docker_id = subprocess.check_output(['docker', 'run', '-d', 'radicale-under-test']).decode().strip()
+    subprocess.check_call(['docker', 'build', '-t', 'radicale-under-test', '.'])
+    docker_id = subprocess.check_output([
+        'docker', 'run', '-d',
+        '--init',
+        '--read-only',
+        '--security-opt=no-new-privileges:true',
+        # Not able to use cap-drop=all and make the container start
+        # '--cap-drop', 'ALL', '--cap-add', 'SYS_ADMIN', '--cap-add', 'CHOWN', '--cap-add', 'SETUID', '--cap-add', 'SETGID', '--cap-add', 'KILL',
+        '--pids-limit', '50',
+        '--memory', '256M',
+        'radicale-under-test'
+    ]).decode().strip()
 
     yield testinfra.get_host("docker://" + docker_id)
 
     # teardown
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
 
+
 def test_process(host):
     process = host.process.get(comm='radicale')
-    assert process.pid == 1
+    assert process.pid != 1
     assert process.user == 'radicale'
     assert process.group == 'radicale'
+
 
 def test_port(host):
     assert host.socket('tcp://0.0.0.0:5232').is_listening
 
+
 def test_version(host):
     assert host.check_output('radicale --version') == '2.1.11'
 
+
 def test_user(host):
     user = 'radicale'
-    assert host.user(user).uid == 6666
-    assert host.user(user).gid == 7777
+    assert host.user(user).uid == 2999
+    assert host.user(user).gid == 2999
     assert host.user(user).shell == '/bin/false'
     assert 'radicale L ' in host.check_output('passwd --status radicale')
+
 
 def test_config_readonly(host):
     config_file = '/config/config'
     assert host.file(config_file).user == 'root'
     assert host.file(config_file).group == 'root'
     assert host.file(config_file).mode == 0o664
+
 
 def test_data_folder_writable(host):
     folder = '/data'
