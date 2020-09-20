0c36e590585e5d221cc69dbe58a871220e85e7cf
Dylan Van Assche <me@dylanvanassche.be>
2020-09-20 13:15:51 +0200

    Integrate git cloning and pushing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,15 @@ if [ -n "$UID" ]; then
     usermod -o -u "$UID" radicale
 fi
 
+# Clone Git repository if enabled
+if [ -n "$GIT_REPOSITORY" ] && [ -n "$GIT_USERNAME" ] && [ -n "$GIT_EMAIL" ]; then
+    git clone "$GIT_REPOSITORY" /data/collections
+
+    # Git needs to know an identity for pushing changes
+    git config --system user.name "$GIT_USERNAME"
+    git config --system user.email "$GIT_EMAIL"
+fi
+
 # Re-set permission to the `radicale` user if current user is root
 # This avoids permission denied if the data volume is mounted by root
 if [ "$1" = 'radicale' ] && [ "$(id -u)" = '0' ]; then
