41cddd9ad9921011f4282c493624749e5d390fa3
Thomas Queste <tom@tomsquest.com>
2021-04-11 16:32:27 +0200

    style: format

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,21 +5,21 @@ set -e
 # Change uid/gid of radicale if vars specified
 if [ -n "$UID" ] || [ -n "$GID" ]; then
     if [ ! "$UID" = "$(id radicale -u)" ] || [ ! "$GID" = "$(id radicale -g)" ]; then
-      # Fail on read-only container
-      if grep -e "\s/\s.*\sro[\s,]" /proc/mounts > /dev/null; then
-          echo "You specified custom UID/GID (UID: $UID, GID: $GID)."
-          echo "UID/GID can only be changed when not running the container with --read-only."
-          echo "Please see the README.md for how to proceed and for explanations."
-          exit 1
-      fi
+        # Fail on read-only container
+        if grep -e "\s/\s.*\sro[\s,]" /proc/mounts > /dev/null; then
+            echo "You specified custom UID/GID (UID: $UID, GID: $GID)."
+            echo "UID/GID can only be changed when not running the container with --read-only."
+            echo "Please see the README.md for how to proceed and for explanations."
+            exit 1
+        fi
 
-      if [ -n "$UID" ]; then
-          usermod -o -u "$UID" radicale
-      fi
+        if [ -n "$UID" ]; then
+            usermod -o -u "$UID" radicale
+        fi
 
-      if [ -n "$GID" ]; then
-          groupmod -o -g "$GID" radicale
-      fi
+        if [ -n "$GID" ]; then
+            groupmod -o -g "$GID" radicale
+        fi
     fi
 fi
 
