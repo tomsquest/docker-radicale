5fa61a74b5ffb9b89e8343276b28d319392381ae
Thomas Queste <tom@tomsquest.com>
2022-01-19 06:33:40 +0100

    chore: upgrade to radicale 3.1.1 (#116)

--- test_image_prod.py
+++ test_image_prod.py
@@ -42,7 +42,7 @@ def test_port(host):
 
 
 def test_version(host):
-    assert host.check_output('radicale --version') == '3.1.0'
+    assert host.check_output('radicale --version') == '3.1.1'
 
 
 def test_user(host):
