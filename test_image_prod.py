d7485703e3257fa7a5c1f1a76c10b33a44f24aa6
Thomas Queste <tom@tomsquest.com>
2025-04-09 06:27:48 +0200

    chore: update to Radicale 3.5.1

--- test_image_prod.py
+++ test_image_prod.py
@@ -51,7 +51,7 @@ def test_port(host):
 
 
 def test_version(host):
-    assert host.check_output("/venv/bin/radicale --version") == "3.5.0"
+    assert host.check_output("/venv/bin/radicale --version") == "3.5.1"
 
 
 def test_user(host):
