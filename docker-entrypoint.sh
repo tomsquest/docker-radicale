b805e56a35679c0475666c35c9d729859f5e8901
Thomas Queste <tom@tomsquest.com>
2025-11-16 15:43:28 +0100

    fix: do not try to update config from env var given readonly container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+# Check if container is running with --read-only
+IS_READONLY=$(grep -e "\s/\s.*\sro[\s,]" /proc/mounts > /dev/null && echo "true" || echo "false")
+
 # Change uid/gid of radicale if vars specified
 if [ -n "$UID" ] || [ -n "$GID" ]; then
     if [ ! "$UID" = "$(id radicale -u)" ] || [ ! "$GID" = "$(id radicale -g)" ]; then
         # Fail on read-only container
-        if grep -e "\s/\s.*\sro[\s,]" /proc/mounts > /dev/null; then
+        if [ "$IS_READONLY" = "true" ]; then
             echo "You specified custom UID/GID (UID: $UID, GID: $GID)."
             echo "UID/GID can only be changed when not running the container with --read-only."
             echo "Please see the README.md for how to proceed and for explanations."
@@ -23,13 +26,19 @@ if [ -n "$UID" ] || [ -n "$GID" ]; then
     fi
 fi
 
+# Update config from Env
+# Fail gracefully if running with --read-only
+if [ "$IS_READONLY" = "true" ]; then
+    echo "Environment variable-based config update is disabled because the container is running with --read-only."
+    echo "If you need to update config from environment variables, remove the --read-only flag."
+else
+    /venv/bin/python /usr/local/bin/update_config_from_env.py
+fi
+
 # If requested and running as root, mutate the ownership of bind-mounts
 if [ "$(id -u)" = "0" ] && [ "$TAKE_FILE_OWNERSHIP" = "true" ]; then
     chown -R radicale:radicale /data
 fi
-
-# Update configuration from environment variables
-/venv/bin/python /usr/local/bin/update_config_from_env.py
 
 # Run radicale as the "radicale" user or any other command if provided
 if [ "$(id -u)" = "0" ] && [ "$1" = "/venv/bin/radicale" ]; then
