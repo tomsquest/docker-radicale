ea2df1031b17f372dcdc8ea70ac316d7b91b6747
tomsquest <365778+tomsquest@users.noreply.github.com>
2026-09-04 15:35:38 +0000

    chore: update to Radicale 3.7.7

--- test_image_prod.py
+++ test_image_prod.py
@@ -52,7 +52,7 @@ def test_port(host):
 
 
 def test_version(host):
-    assert host.check_output("/venv/bin/radicale --version") == "3.7.6"
+    assert host.check_output("/venv/bin/radicale --version") == "3.7.7"
 
 
 def test_user(host):
